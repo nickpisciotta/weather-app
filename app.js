51a26772782dd39470e2afcf952b3341c8499bf8
Nick Pisciotta <npisciotta@gmail.com>
2017-01-09 16:02:38 -0700

    Retrieve weather info from geocode data Include catch statement for all errors

--- app.js
+++ app.js
@@ -19,6 +19,32 @@ var encodedAddress = encodeURIComponent(argv.address);
 var geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`; 
 
 axios.get(geocodeURL).then((response) => {
-  
-}); 
+  if (response.data.status === "ZERO_RESULTS") {
+    throw new Error("Unable to find that address"); 
+  }
+  sendGeocodeToWeatherAPI(response).then((response) => {
+    displayWeatherData(response); 
+  })
+}).catch((error) => {
+    if (error.code === 'ENOTFOUND') {
+      console.log("Could not connect to API server"); 
+    } else {
+      console.log(error.message);  
+    }
+});  
+
+const sendGeocodeToWeatherAPI = (geocodeData) => { 
+  var lat = geocodeData.data.results[0].geometry.location.lat; 
+  var long = geocodeData.data.results[0].geometry.location.lng;
+  console.log(geocodeData.data.results[0].formatted_address)
+  var weatherURL = `https://api.darksky.net/forecast/${process.env.DARKSKYAPI}/${lat},${long}`
+  return axios.get(weatherURL); 
+}
+
+const displayWeatherData = (weatherData) => {
+  var temperature = weatherData.data.currently.temperature; 
+  var apparentTemperature = weatherData.data.currently.apparentTemperature
+  console.log(`It's currently: ${temperature}.  It feels like: ${apparentTemperature}`)
+}
+
 
